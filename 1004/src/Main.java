61e5c153a17bb9fccb3bac8d199264de678d9647
Ray Fang <i@lazarusx.com>
2015-05-25 21:02:56 +0800

    Add solution to Problem 1004

---
+++ 1004/src/Main.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/**
+ * Created by LazarusX on 15/5/25.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+        double sum = 0;
+        for (int i = 0; i < 12; i++) {
+            sum += cin.nextDouble();
+        }
+
+        System.out.printf("$%.2f\n", sum / 12);
+    }
+}
